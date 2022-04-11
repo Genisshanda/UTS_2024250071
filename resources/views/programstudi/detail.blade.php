4459ab195b8dc08fcd0f842c4e07e7c6a92a1f5b
Genisshanda <100113812+Genisshanda@users.noreply.github.com>
2022-04-11 19:53:06 +0700

    Create detail.blade.php

---
+++ resources/views/programstudi/detail.blade.php
@@ -0,0 +1,14 @@
+@include("layout.headerdetailprogram")
+
+
+<h1> Halaman Detail Matakuliah</h1>
+
+<ol>
+@foreach ($programstudi as $key => $value)
+
+
+
+@endforeach
+</ol>
+
+@include("layout.footer")
