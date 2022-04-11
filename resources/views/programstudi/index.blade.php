3b30c81d3799476d02db572bf30318a2b71098a2
Genisshanda <100113812+Genisshanda@users.noreply.github.com>
2022-04-11 19:53:08 +0700

    Create index.blade.php

---
+++ resources/views/programstudi/index.blade.php
@@ -0,0 +1,14 @@
+@include("layout.headerprogramstudi")
+
+
+<h1> Halaman List  Programstudi</h1>
+
+<ol>
+@foreach ($programstudi as $key => $value)
+
+
+
+@endforeach
+</ol>
+
+@include("layout.footer")
