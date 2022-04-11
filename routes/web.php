cd3267723e923e00bbca1aec707a77b9fe6af736
Genisshanda <100113812+Genisshanda@users.noreply.github.com>
2022-04-11 19:53:19 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,41 @@ Route::get('/', function () {
 Route :: get("/matakuliah", function(){
     $judul="Halaman list Mata Kuliah : Menampilkan seluruh data Mata Kuliah dan Nama Program
     Studi";
-    $matakuliah=['mtl','haha'];
+
+    $matakuliah=['PWII','haha'];
     
      return view("matakuliah.index",compact('matakuliah','judul'));
   });
+
+
+  Route :: get("/matakuliah/detail", function(){
+    
+    return view("matakuliah.detail")->with("matakuliah",["PWII"]);
+ }); 
+
+
+ Route :: get("/programstudi", function(){
+   
+    $programstudi=['PWII','PWI'];
+    
+     return view("programstudi.index",compact('programstudi'));
+  });
+
+
+  Route :: get("/programstudi/detail", function(){
+    
+    return view("programstudi.detail")->with("programstudi",['Teknik Sipil','Teknik mesin', 'Teknik Kimia', 'Teknik Electro', 'Teknik Fisika']);
+ }); 
+
+
+ use App\Http\Controllers\MatakuliahController;
+
+
+Route :: get("/matakuliah", [MatakuliahController::class,'index']);
+Route :: get("/matakuliah/detail/{id?}", [MatakuliahController::class,'detail']);
+
+use App\Http\Controllers\ProgramstudiController;
+
+
+Route :: get("/programstudi", [ProgramstudiController::class,'index']);
+Route :: get("/programstudi/detail/{id?}", [ProgramstudiController::class,'detail']);
