fe200210a2249d56583e18c30142acf1f8fc5ae8
Genisshanda <100113812+Genisshanda@users.noreply.github.com>
2022-04-11 19:53:03 +0700

    Create detail.blade.php

---
+++ resources/views/matakuliah/detail.blade.php
@@ -0,0 +1,14 @@
+@include("layout.headerdateailmatkul")
+
+
+<h1> Halaman Detail Matakuliah</h1>
+
+<ol>
+@foreach ($matakuliah as $key => $value)
+
+
+
+@endforeach
+</ol>
+
+@include("layout.footer")
